5dce7caa7ee21a35010c1e82eab573bb1555500d
AminaSultana08 <aminasultana088@gmail.com>
2023-10-21 19:30:14 +0600

    updated product added to database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,8 +10,7 @@ app.use(express.json())
 
 
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
+
 
  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wua58o9.mongodb.net/?retryWrites=true&w=majority`;
  console.log(uri);
@@ -28,19 +27,67 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db('productDB').collection('product')
+    const userCollection = client.db('userDB').collection('user')
 
+    app.get('/product',async(req,res)=>{
+      const cursor = productCollection.find()
+      const result = await cursor.toArray()
+      res.send(result) 
+    })
+
+    app.get('/product/:id', async(req,res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await productCollection.findOne(query)
+      res.send(result)
+    })
+
+   
+
+    
+//product
     app.post ('/product', async(req,res)=>{
       const newProduct = req.body
       console.log(newProduct);
 
+//user
+      app.post('/user',async(req,res)=>{
+        const user = req.body
+        console.log(user);
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+      } )
+
       const result = await productCollection.insertOne(newProduct)
       res.send(result)
     })
+
+//product update
+    app.put('/product/:id', async(req,res)=>{
+      const id = req.params.id
+      const filter = {_id : new ObjectId(id)}
+      const options = {upsert : true}
+      const updatedProduct = req.body
+      const product ={
+        $set:{
+          name:updatedProduct.name,
+          photo:updatedProduct.photo,
+          brand_name:updatedProduct.brand_name,
+          type :updatedProduct.type,
+          price:updatedProduct.price,
+          description:updatedProduct.description,
+          rating:updatedProduct.rating,
+
+        }
+      }
+      const result = await productCollection.updateOne(filter,product,options)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
